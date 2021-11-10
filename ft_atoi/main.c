c4f9aa06f030e85178297d8b7f450b1dd6073ec9
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-10 17:26:55 +0200

    ft_atoi removed extra braces

--- ft_atoi/main.c
+++ ft_atoi/main.c
@@ -6,13 +6,13 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 17:58:29 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/09 20:56:00 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/10 17:25:57 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_tests.h"
-#include "../_test_ft/includes/test_ft.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 int	ft_test(char *n)
 {
@@ -22,36 +22,32 @@ int	ft_test(char *n)
 	i1 = atoi(n);
 	i2 = ft_atoi(n);
 	if (0)
-	{
-		ft_putnbr_test(i1);
-		ft_putchar_test(' ');
-		ft_putnbr_test(i2);
-		ft_putchar_test('\n');
-	}
+		printf("%d %d\n", i1, i2);
 	return (i1 == i2);
 }
 
 int	main(void)
 {
-	ft_atoi(NULL);
-	if (ft_test("0")
-		&& ft_test("-0")
-		&& ft_test("+0")
-		&& ft_test("-")
-		&& ft_test("+")
-		&& ft_test("a")
-		&& ft_test("+a")
-		&& ft_test("-b")
-		&& ft_test("7")
-		&& ft_test("+7a3")
-		&& ft_test("-6O4")
-		&& ft_test("++10")
-		&& ft_test("--10")
-		&& ft_test("2147483647")
-		&& ft_test("-2147483648")
-		&& ft_test("1000000000000")
-		&& ft_test("-1000000000000"))
-		ft_putstr_test("OK: ft_atoi");
-	else
-		ft_putstr_test("KO: ft_atoi");
+	if (!ft_test("0")
+		&& !ft_test("-0")
+		&& !ft_test("+0")
+		&& !ft_test("-")
+		&& !ft_test("+")
+		&& !ft_test("a")
+		&& !ft_test("+a")
+		&& !ft_test("-b")
+		&& !ft_test("7")
+		&& !ft_test("+7a3")
+		&& !ft_test("-6O4")
+		&& !ft_test("++10")
+		&& !ft_test("--10")
+		&& !ft_test("2147483647")
+		&& !ft_test("-2147483648")
+		&& !ft_test("1000000000000")
+		&& !ft_test("-1000000000000"))
+	{
+		printf("KO: ft_atoi");
+		return (1);
+	}
+	printf("OK: ft_atoi");
 }
