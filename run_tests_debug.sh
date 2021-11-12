5de1be2bea3ea8abc07d993870672854cf61c6be
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-12 12:39:32 +0200

    run_tests_debug shell script

---
+++ run_tests_debug.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+partone="ft_memset
+ft_bzero
+ft_memcpy
+ft_memccpy
+ft_memmove
+ft_memchr
+ft_memcmp
+ft_strlen
+ft_strdup
+ft_strcpy
+ft_strncpy
+ft_strcat
+ft_strncat
+ft_strlcat
+ft_strchr
+ft_strrchr
+ft_strstr
+ft_strnstr
+ft_strcmp
+ft_strncmp
+ft_atoi
+ft_isalpha
+ft_isdigit
+ft_isalnum
+ft_isascii
+ft_isprint
+ft_toupper
+ft_tolower"
+
+parttwo="ft_memalloc
+ft_memdel
+ft_strnew
+ft_strdel
+ft_strclr
+ft_striter
+ft_striteri
+ft_strmap
+ft_strmapi
+ft_strequ
+ft_strnequ
+ft_strsub
+ft_strjoin
+ft_strtrim
+ft_strsplit
+ft_itoa
+ft_putchar
+ft_putstr
+ft_putendl
+ft_putnbr
+ft_putchar_fd
+ft_putstr_fd
+ft_putendl_fd
+ft_putnbr_fd"
+
+partbonus="ft_lstnew
+ft_lstdelone
+ft_lstdel
+ft_lstadd
+ft_lstiter
+ft_lstmap"
+
+for value in $partone; do
+	norminette ../libft/$value.c
+	make $value"_debug"
+	cd ./_programs/$value/
+	./$value
+	cd ../../
+	echo ""
+done
