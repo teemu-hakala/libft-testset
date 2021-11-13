b7a13955da933aaabeb22981f55f87a77cbf0caa
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-13 18:13:55 +0200

    ft_bzero segfault test automation

--- ft_bzero/main.c
+++ ft_bzero/main.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 04:48:33 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/12 10:45:00 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/13 18:11:16 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
+#include <signal.h>
+#include <setjmp.h>
+#define _LIBC_MSG " libc"
+#define LIBFT_MSG "libft"
 
-void	ft_test_int_array(int *std_array, size_t len)
+char	*g_message;
+int		g_std_segfault;
+jmp_buf	g_buffer;
+int		g_print;
+
+static int	ft_test_int_array(int *std_array, size_t len)
 {
 	int		*ft_array;
 
@@ -24,14 +33,17 @@ void	ft_test_int_array(int *std_array, size_t len)
 	memcpy(ft_array, std_array, len);
 	ft_bzero(ft_array, len);
 	bzero(std_array, len);
-	if (!memcmp(ft_array, std_array, len))
-		printf("OK: ft_bzero int array\n");
-	else
+	if (memcmp(ft_array, std_array, len))
+	{
 		printf("KO: ft_bzero int array\n");
+		free(ft_array);
+		return (1);
+	}
 	free(ft_array);
+	return (0);
 }
 
-void	ft_test_str(char *std_s, size_t len)
+static int	ft_test_str(char *std_s, size_t len)
 {
 	char	*ft_s;
 
@@ -39,17 +51,82 @@ void	ft_test_str(char *std_s, size_t len)
 	strncpy(ft_s, std_s, len);
 	ft_bzero(ft_s, len);
 	bzero(std_s, len);
-	if (!memcmp(ft_s, std_s, len))
-		printf("OK: ft_bzero string\n");
-	else
+	if (memcmp(ft_s, std_s, len))
+	{
 		printf("KO: ft_bzero string\n");
+		free(ft_s);
+		free(std_s);
+		return (1);
+	}
 	free(ft_s);
 	free(std_s);
+	return (0);
+}
+
+static int	ft_segfault(void *ptr, size_t count, size_t dbg)
+{
+	int		jmp_result;
+	int		truth;
+
+	jmp_result = setjmp(g_buffer);
+	truth = 1;
+	if (g_print)
+		printf("jump%zu: %d\n", dbg, jmp_result);
+	if (!jmp_result)
+	{
+		strcpy(g_message, _LIBC_MSG);
+		bzero(ptr, count);
+		g_std_segfault = 0;
+	}
+	jmp_result = setjmp(g_buffer);
+	if (g_print)
+		printf("jump%zu: %d\n", dbg, jmp_result);
+	if (!jmp_result)
+	{
+		strcpy(g_message, LIBFT_MSG);
+		ft_bzero(ptr, count);
+		truth = !g_std_segfault;
+		g_std_segfault = 0;
+	}
+	return (truth);
+}
+
+static void	segfault_sigaction(int signal, siginfo_t *si, void *arg)
+{
+	(void)signal;
+	(void)si;
+	(void)arg;
+	if (g_print)
+		printf("SIGSEGV: %s\n", g_message);
+	g_std_segfault = 1;
+	longjmp(g_buffer, 19);
 }
 
 int	main(void)
 {
-	ft_test_int_array((int []){1, 2, 3, 4, 5, 6, 7, 8}, sizeof(int) * 8);
-	ft_test_str(strdup("Ten chars"), 10);
+	struct sigaction	sa;
+	size_t				id;
+
+	memset(&sa, 0, sizeof(struct sigaction));
+	sigemptyset(&sa.sa_mask);
+	sa.sa_sigaction = segfault_sigaction;
+	sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGSEGV, &sa, NULL);
+	id = 0;
+	g_print = 0;
+	g_message = strdup(_LIBC_MSG);
+	if (ft_test_int_array((int []){1, 2, 3, 4, 5, 6, 7, 8}, sizeof(int) * 8)
+		|| ft_test_str(strdup("Ten chars"), 10))
+	{
+		printf("KO: ft_bzero\n");
+		return (1);
+	}
+	else if (ft_segfault(NULL, (size_t)0, id++)
+		|| ft_segfault(NULL, (size_t)1, id++))
+	{
+		printf("KO: ft_bzero: SIGSEGV diff %zu\n", id);
+	}
+	printf("OK: ft_bzero\n");
+	free(g_message);
 	return (0);
 }
