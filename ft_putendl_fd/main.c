c3374f993718b115c5d28e8241801619e02e378c
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-17 19:56:11 +0200

    ft_putendln more rigor

--- ft_putendl_fd/main.c
+++ ft_putendl_fd/main.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 16:21:52 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/13 14:52:01 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/17 19:55:23 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+
+static void	*memdup(const void *mem, size_t size)
+{
+	void	*out;
+
+	out = malloc(size);
+	if (out)
+		memcpy(out, mem, size);
+	return (out);
+}
 
 static int	ft_test_rdwr(char *str, int fd)
 {
@@ -26,17 +36,22 @@ static int	ft_test_rdwr(char *str, int fd)
 
 	ft_putendl_fd(str, fd);
 	size = strlen(str);
-	read_str = (char *)malloc(sizeof(char) * (size + (size_t)2));
-	strnl = (char *)malloc(sizeof(char) * (strlen(str) + 2));
-	if (!read_str || !strnl)
+	read_str = (char *)malloc(sizeof(char) * (size + (size_t)3));
+	if (!read_str)
 	{
 		printf("Memory allocation error!\n");
 		free(read_str);
-		free(strnl);
+		return (1);
+	}
+	memset(read_str, '\1', size + 3);
+	strnl = (char *)memdup(read_str, size + 3);
+	if (!strnl)
+	{
+		free(read_str);
 		return (1);
 	}
 	lseek(fd, 0, SEEK_SET);
-	rb = read(fd, read_str, size + 1);
+	rb = read(fd, read_str, size + 2);
 	if (rb == -1)
 	{
 		printf("File read error.\n");
@@ -44,8 +59,10 @@ static int	ft_test_rdwr(char *str, int fd)
 		free(strnl);
 		return (1);
 	}
+	if ((size_t)rb > size + 1)
+		return (1);
 	read_str[rb] = '\0';
-	result = strcmp(strcat(strcpy(strnl, str), "\n"), read_str);
+	result = memcmp(strcat(strcpy(strnl, str), "\n"), read_str, size + 3);
 	free(read_str);
 	free(strnl);
 	return (result);
@@ -54,6 +71,7 @@ static int	ft_test_rdwr(char *str, int fd)
 static int	ft_test(char *str)
 {
 	int		fd;
+	int		result;
 
 	fd = open("ft_putendl_fd.output",
 			O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
@@ -62,10 +80,10 @@ static int	ft_test(char *str)
 		printf("File open error.\n");
 		return (1);
 	}
-	ft_test_rdwr(str, fd);
+	result = ft_test_rdwr(str, fd);
 	if (close(fd) == -1)
 		printf("File close error.\n");
-	return (0);
+	return (result);
 }
 
 int	main(void)
