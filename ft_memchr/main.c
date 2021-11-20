e4959fb86de7a168e279c907dfa87897fab5f23d
Teemu Hakala <thakala@c3r5p4.hive.fi>
2021-11-20 19:10:55 +0200

    ft_memchr main re-added nul terminator location test

--- ft_memchr/main.c
+++ ft_memchr/main.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 13:49:17 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/20 19:06:48 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/20 19:10:01 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ int	main(void)
 {
 	if (!ft_test("Test string.", 's')
 		|| !ft_test("Test string.", '.')
+		|| !ft_test("Test string.", '\0')
 		|| !ft_test_diff("Test str\0ng.", '\0', 13))
 	{
 		printf("KO: ft_memchr\n");
