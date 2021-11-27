fb71d491d33bf4d6ddcc6ac70127984166f92892
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-27 11:32:31 +0200

    ft_putnbr_fd main printf transformation

--- ft_putnbr_fd/main.c
+++ ft_putnbr_fd/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
+/*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 09:02:39 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/04 17:20:58 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/27 11:31:09 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int	ft_test(int written_int)
 {
@@ -25,25 +26,23 @@ int	ft_test(int written_int)
 			O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
-		ft_putstr_test("File open error.\n");
+		printf("File open error.\n");
 		return (1);
 	}
 	ft_putnbr_fd(written_int, fd);
 	lseek(fd, 0, SEEK_SET);
 	if (read(fd, &read_int, 11) == -1)
 	{
-		ft_putstr_test("File read error.\n");
+		printf("File read error.\n");
 		close(fd);
 		return (1);
 	}
-	if (written_int == ft_atoi_test(read_int))
-		ft_putstr_test("OK: ft_putnbr_fd\n");
+	if (written_int == atoi(read_int))
+		printf("OK: ft_putnbr_fd\n");
 	else
 	{
-		ft_putstr_test("KO: ft_putnbr_fd\n");
-		ft_putstr_test("read byte: ");
-		ft_putnbr_test(ft_atoi_test(read_int));
-		ft_putchar_test('\n');
+		printf("KO: ft_putnbr_fd\n");
+		printf("read byte: %d\n", atoi(read_int));
 	}
 	close(fd);
 	return (0);
@@ -51,6 +50,10 @@ int	ft_test(int written_int)
 
 int	main(void)
 {
-	if (ft_test(4))
+	if (ft_test(0)
+		|| ft_test(4)
+		|| ft_test((int)(~(~0u >> 1)))
+		|| ft_test((int)((~0u >> 1)))
+		|| ft_test(1000000000))
 		return (1);
 }
