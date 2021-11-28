7a8ef8b3541eb87b25e935de0dd208d4b749dd21
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-28 16:56:12 +0200

    ft_strpbrk main

---
+++ ft_strpbrk/main.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/28 16:37:17 by thakala           #+#    #+#             */
+/*   Updated: 2021/11/28 16:55:52 by thakala          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ft_tests.h"
+
+static int	ft_test(const char *origin, const char *charset)
+{
+	return (strpbrk(origin, charset) == ft_strpbrk(origin, charset));
+}
+
+int	main(void)
+{
+	if (ft_test("", "")
+		|| ft_test("", "I")
+		|| ft_test(" ", "")
+		|| ft_test(" ", "l")
+		|| ft_test("ove", "")
+		|| ft_test("ove", "U")
+		|| ft_test("a", "a")
+		|| ft_test("lw", "w")
+		|| ft_test("ays", "y")
+		|| ft_test("tripletest", "t")
+		|| ft_test("doubletest", "t")
+		|| ft_test("odd", "d")
+		|| ft_test("quadrupletest", "quadrplets")
+		|| ft_test("tokyo", "toy")
+		|| ft_test("Beast in Black", "Beast")
+		|| ft_test("Beast in B", "Beast in"))
+	{
+		printf("KO: ft_strpbrk\n");
+		return (1);
+	}
+	printf("OK: ft_strpbrk\n");
+	return (0);
+}
