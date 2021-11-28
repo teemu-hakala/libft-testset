742257b6c3f3f23b04330b55be6a56de4f6a55f3
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-28 16:37:01 +0200

    ft_strsep main written out

--- ft_strsep/main.c
+++ ft_strsep/main.c
@@ -6,12 +6,13 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 14:02:39 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/28 14:34:31 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/28 16:01:09 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_tests.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 static int	ft_test(const char *base, const char *delim)
 {
@@ -19,14 +20,30 @@ static int	ft_test(const char *base, const char *delim)
 	char	*ft_string;
 	char	*beginning_std;
 	char	*beginning_ft;
+	char	*token_std;
+	char	*token_ft;
 
 	std_string = strdup(base);
+	if (!std_string)
+	{
+		printf("Memory allocation error.\n");
+		return (1);
+	}
 	ft_string = strdup(std_string);
+	if (!ft_string)
+	{
+		printf("Memory allocation error.\n");
+		return (1);
+	}
 	beginning_std = std_string;
 	beginning_ft = ft_string;
 	while (1)
 	{
-		if (strcmp(strsep(&std_string, delim), ft_strsep(&ft_string, delim)))
+		token_std = strsep(&std_string, delim);
+		token_ft = ft_strsep(&ft_string, delim);
+		if (!token_std && !token_ft)
+			break ;
+		if (strcmp(token_std, token_ft))
 		{
 			free(beginning_std);
 			free(beginning_ft);
@@ -44,6 +61,7 @@ int	main(void)
 		|| ft_test("", "a")
 		|| ft_test("", "")
 		|| ft_test("M'envoler vers toi", " ")
+		|| ft_test("I love you!", "!")
 		|| ft_test("I STOP love STOP you STOP", " STOP"))
 	{
 		printf("KO: ft_strsep\n");
