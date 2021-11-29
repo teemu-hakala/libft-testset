86ffc89ff008120f17a838d254a17b457126dd26
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-29 09:23:42 +0200

    ft_strcspn main

---
+++ ft_strcspn/main.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/28 19:44:19 by thakala           #+#    #+#             */
+/*   Updated: 2021/11/29 09:23:09 by thakala          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int	ft_test(const char *string, const char *charset)
+{
+	return (strcspn(string, charset) != ft_strcspn(string, charset));
+}
+
+int	main(void)
+{
+	if (ft_test("", "")
+		|| ft_test(" ", "")
+		|| ft_test("", "g")
+		|| ft_test(" ", "g")
+		|| ft_test("w", "w")
+		|| ft_test("w", ":")
+		|| ft_test("ijk", "pqr")
+		|| ft_test("mno", "o")
+		|| ft_test("set", "yws")
+		|| ft_test("long\0er char star", "er charst"))
+	{
+		printf("KO: ft_strcspn\n");
+		return (1);
+	}
+	printf("OK: ft_strcspn\n");
+	return (0);
+}
