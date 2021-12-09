6723195314e4e87a001edc503db37ccb1ae69197
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-12-09 19:54:35 +0200

    ft_strnstr amajer fix (heap allocated strings)

--- ft_strnstr/main.c
+++ ft_strnstr/main.c
@@ -3,24 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
+/*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 15:57:16 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/10 16:00:13 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/09 18:58:30 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_tests.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 static int	ft_test(const char *haystack, const char *needle, size_t len)
 {
+	char	*haystack_heap;
+	char	*needle_heap;
 	char	*std_result;
 	char	*ft_result;
 
-	std_result = strnstr(haystack, needle, len);
-	ft_result = ft_strnstr(haystack, needle, len);
+	haystack_heap = (char *)malloc(sizeof(char) * (strlen(haystack) + 1));
+	needle_heap = (char *)malloc(sizeof(char) * (strlen(needle) + 1));
+	std_result = strnstr(haystack_heap, needle_heap, len);
+	ft_result = ft_strnstr(haystack_heap, needle_heap, len);
+	free(haystack_heap);
+	free(needle_heap);
 	return (std_result == ft_result);
 }
 
