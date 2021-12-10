10d9bc9e55c47f8df399f33dae9acde64a8d8fdb
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-12-10 12:37:11 +0200

    ft_sign main

--- ft_sign/main.c
+++ ft_sign/main.c
@@ -6,16 +6,16 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 11:26:12 by thakala           #+#    #+#             */
-/*   Updated: 2021/12/10 11:32:22 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/10 11:44:06 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_tests.h"
 #include <stdio.h>
 
-static int	ft_test(value, reference)
+static int	ft_test(long value, char reference)
 {
-	return (ft_sign(value) != (char)reference);
+	return (ft_sign(value) != reference);
 }
 
 int	main(void)
