5bc9b1489e7af3022254eb86d844376e6deb8b4d
teemu-hakala <78655964+teemu-hakala@users.noreply.github.com>
2021-12-22 18:10:29 +0200

    ft_strequ main heap allocated strings

--- ft_strequ/main.c
+++ ft_strequ/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
+/*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 18:50:45 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/14 18:56:29 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/22 18:09:48 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,23 @@
 
 static int	ft_test(const char *s1, const char *s2, int expected)
 {
-	return (ft_strequ(s1, s2) == expected);
+	char	*heap_s1;
+	char	*heap_s2;
+	int		result;
+
+	heap_s1 = (char *)malloc(sizeof(char) * (strlen(s1) + 1));
+	heap_s2 = (char *)malloc(sizeof(char) * (strlen(s2) + 1));
+	if (!heap_s1 || !heap_s2)
+	{
+		printf("Memory allocation error.\n");
+		return (1);
+	}
+	ft_strcpy(heap_s1, s1);
+	ft_strcpy(heap_s2, s2);
+	result = ft_strequ(heap_s1, heap_s2) == expected;
+	free(heap_s1);
+	free(heap_s2);
+	return (result);
 }
 
 int	main(void)
