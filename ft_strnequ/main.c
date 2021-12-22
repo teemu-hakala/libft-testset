0fdd060edf6fa2684dc199916347e927a60159e1
teemu-hakala <78655964+teemu-hakala@users.noreply.github.com>
2021-12-22 18:13:36 +0200

    ft_strnequ main heap allocated memory

--- ft_strnequ/main.c
+++ ft_strnequ/main.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 19:00:57 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/30 17:54:40 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/22 18:13:01 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,53 @@
 
 static int	ft_test(const char *s1, const char *s2, size_t n, int expected)
 {
-	return (ft_strnequ(s1, s2, n) == expected);
+	char	*heap_s1;
+	char	*heap_s2;
+	int		result;
+
+	heap_s1 = (char *)malloc(sizeof(char) * (strlen(s1) + 1));
+	heap_s2 = (char *)malloc(sizeof(char) * (strlen(s2) + 1));
+	if (!heap_s1 || !heap_s2)
+	{
+		printf("Memory allocation error.\n");
+		return (1);
+	}
+	ft_strcpy(heap_s1, s1);
+	ft_strcpy(heap_s2, s2);
+	result = ft_strnequ(s1, s2, n) == expected;
+	free(heap_s1);
+	free(heap_s2);
+	return (result);
 }
 
 static int	ft_test_loop(const char *s1, const char *s2, size_t upto)
 {
+	char	*heap_s1;
+	char	*heap_s2;
+	int		result;
+
+	heap_s1 = (char *)malloc(sizeof(char) * (strlen(s1) + 1));
+	heap_s2 = (char *)malloc(sizeof(char) * (strlen(s2) + 1));
+	if (!heap_s1 || !heap_s2)
+	{
+		printf("Memory allocation error.\n");
+		return (1);
+	}
+	ft_strcpy(heap_s1, s1);
+	ft_strcpy(heap_s2, s2);
+	result = 1;
 	upto += 2;
 	while (--upto)
+	{
 		if (ft_strnequ(s1, s2, upto - 1) == !!strncmp(s1, s2, upto - 1))
-			return (0);
-	return (1);
+		{
+			result = 0;
+			break ;
+		}
+	}
+	free(heap_s1);
+	free(heap_s2);
+	return (result);
 }
 
 int	main(void)
